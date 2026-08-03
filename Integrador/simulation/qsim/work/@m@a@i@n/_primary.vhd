61a1a2f00f32df52cad915cfcd992b6e16e18748
Leø <leonelvillca0@gmail.com>
2026-08-03 00:41:22 -0300

    entregado

--- Integrador/simulation/qsim/work/@m@a@i@n/_primary.vhd
+++ Integrador/simulation/qsim/work/@m@a@i@n/_primary.vhd
@@ -2,15 +2,14 @@ library verilog;
 use verilog.vl_types.all;
 entity MAIN is
     port(
-        Z               : out    vl_logic;
-        I2              : in     vl_logic;
-        I1              : in     vl_logic;
-        I0              : in     vl_logic;
+        AC              : out    vl_logic_vector(3 downto 0);
+        I               : in     vl_logic_vector(2 downto 0);
+        CLK             : in     vl_logic;
+        LOADA           : in     vl_logic;
         A               : in     vl_logic_vector(3 downto 0);
+        S               : out    vl_logic_vector(3 downto 0);
+        LOADB           : in     vl_logic;
         B               : in     vl_logic_vector(3 downto 0);
-        N               : out    vl_logic;
-        C               : out    vl_logic;
-        V               : out    vl_logic;
-        S               : out    vl_logic_vector(3 downto 0)
+        FLAGS           : out    vl_logic_vector(3 downto 0)
     );
 end MAIN;
--- Integrador/simulation/qsim/work/@m@a@i@n_vlg_sample_tst/_primary.vhd
+++ Integrador/simulation/qsim/work/@m@a@i@n_vlg_sample_tst/_primary.vhd
@@ -4,9 +4,10 @@ entity MAIN_vlg_sample_tst is
     port(
         A               : in     vl_logic_vector(3 downto 0);
         B               : in     vl_logic_vector(3 downto 0);
-        I0              : in     vl_logic;
-        I1              : in     vl_logic;
-        I2              : in     vl_logic;
+        CLK             : in     vl_logic;
+        I               : in     vl_logic_vector(2 downto 0);
+        LOADA           : in     vl_logic;
+        LOADB           : in     vl_logic;
         sampler_tx      : out    vl_logic
     );
 end MAIN_vlg_sample_tst;
